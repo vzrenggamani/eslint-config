020ff662a9e0fe25f2af3afb3f0e65755689fa9a
Rengga Prakoso Nugroho <hi@renggaprakosonugroho.my.id>
2020-09-08 00:17:04 +0700

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+  ],
+  plugins: ['react-hooks'],
+  rules: {
+    'import/extensions': 'off',
+    'import/no-dynamic-require': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-named-as-default': 'off',
+    'import/no-unresolved': 'off',
+    'import/prefer-default-export': 'error',
+    'newline-per-chained-call': 'off',
+    'jsx-a11y/label-has-for': [
+      'warn',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
+    'no-confusing-arrow': 'off',
+    'no-use-before-define': 'off',
+    'prefer-template': 'error',
+    'require-yield': 'off',
+  },
+};
